ee7733fce3ee783145da8bba6bf17a44438be58b
Divyanshi Bhadauria <bdivyanshi11.11@gmail.com>
2024-06-22 10:39:43 +0530

    first

--- C/BubbleSorting.c
+++ C/BubbleSorting.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void bubble_shot(int arr[],int n){
-    int i,j;
+void BubbleSort(int arr[],int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
-                int t=arr[j];
+                int temp=arr[j];
                 arr[j]=arr[j+1];
-                arr[j+1]=t;
+                arr[j+1]=temp;
             }
         }
     }
 }
 
 int main(){
-    int arr[]={65,23,43,67,47,57,45};
+    int arr[]={34,25,76,65,23,43,67,47,57,45};
     int n=sizeof(arr)/sizeof(arr[0]);
-    bubble_shot(arr,n);
+    BubbleSort(arr,n);
     for(int i=0;i<n;i++){
         printf("%d ",arr[i]);
     }

72aaea2b32ff92074cbf17e739a7a45527891758
Divyanshi Bhadauria <bdivyanshi11.11@gmail.com>
2024-07-05 13:59:58 +0530

    first

--- C/QuickSort.c
+++ C/QuickSort.c
@@ -9,46 +9,46 @@ void swap(int *a, int *b)
 
 int partition(int a[], int low, int high)
 {
-    int pivot = a[low];
-    int left = low + 1;
-    int right = high;
-    while (left <= right)
+    int pivot=a[low];
+    int left=low+1;
+    int right=high;
+    while(left<=right)
     {
-        while (left <= right && a[left] <= pivot)
+        while(left<=right && a[left]<=pivot)
         {
             left++;
         }
-        while (left <= right && a[right] > pivot)
+        while(left<=right && a[right]>pivot)
         {
             right--;
         }
-        if (left < right)
+        if(left<right)
         {
             swap(&a[left], &a[right]);
         }
     }
-    swap(&a[low], &a[right]);
+    swap(&a[low],&a[right]);
     return right;
 }
 
 void quickSort(int arr[], int low, int high)
 {
-    if (low < high)
+    if(low<high)
     {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int pi=partition(arr,low,high);
+        quickSort(arr,low,pi-1);
+        quickSort(arr,pi+1,high);
     }
 }
 
 int main()
 {
-    int arr[] = {23, 4, 45, 26, 76, 34};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    quickSort(arr, 0, n - 1);
-    for (int i = 0; i < n; i++)
+    int arr[]={23,4,45,26,76,34};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr,0,n-1);
+    for (int i=0; i<n; i++)
     {
-        printf("%d ", arr[i]);
+        printf("%d ",arr[i]);
     }
     printf("\n");
     return 0;
